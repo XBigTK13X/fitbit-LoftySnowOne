f0782be8a3066ceac7ccc7258f86c07afe4e31d7
Tim Kretschmer <xbigtk13x@gmail.com>
2023-07-31 15:01:31 -0500

    Remove weather. Never worked on a real device

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import clock from "clock";
 import * as document from "document";
-import * as messaging from "messaging";
 import { days, months, monthsShort } from "./locales/en.js";
 import { battery } from "power";
 
@@ -17,17 +16,9 @@ const timeLabel = document.getElementById("timeLabel");
 const amPmLabel = document.getElementById('amPmLabel');
 const dayLabel = document.getElementById("dayLabel");
 const dateLabel = document.getElementById("dateLabel");
-const tempLabel = document.getElementById("tempLabel");
-const weatherLabel = document.getElementById('weatherLabel');
 const batteryLabel = document.getElementById('batteryLabel');
 
-let temp = "";
-let weather = '';
-
 clock.ontick = (evt) => {
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send({action: "weather"})
-  }
   let today = evt.date;
   let hours = today.getHours();
   let postfix = "AM"
@@ -45,24 +36,7 @@ clock.ontick = (evt) => {
   let year = today.getFullYear();
   timeLabel.text = `${hours}:${mins}`;
   amPmLabel.text = postfix;
-  dateLabel.text = `${month}/${dayNumber}/${year}`;
+  dateLabel.text = `${month} / ${dayNumber} / ${year}`;
   dayLabel.text = dayName
-  tempLabel.text = temp
-  weatherLabel.text = weather
   batteryLabel.text = Math.floor(battery.chargeLevel) + "%";
 }
-
-messaging.peerSocket.onmessage = evt => {
-    if (evt && evt.data && evt.data.temp){
-        let unit = evt.data.unit
-        if(unit.indexOf('cel') !== -1){
-            unit = '°C'
-        } else {
-            unit = '°F'
-        }
-        temp = `${evt.data.temp}${unit}`;
-        weather = `${evt.data.condDisplay}`
-        tempLabel.text = temp
-        weatherLabel.text = weather
-    }
-  };
